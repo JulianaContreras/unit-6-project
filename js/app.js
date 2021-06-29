2f2a03fde79a32e63fccc7bff76a4fa6a9c313eb
Juliana Contreras <2022jcontreras@arisehighschool.org>
2021-06-29 11:34:20 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+// variables 
+const qwerty= document.getElementById('qwerty');
+const phrase= document.getElementById('phrase');
+const startGameBtn= document.querySelector('.btn_reset');
+var missed= 0;
+
+// Event Listeners
+
+startGameBtn.addEventListener('click', ()=>{
+   
+});
+
+// Arrays
+
+phrases= [
+    'Animal',
+    'Bunny',
+    'Cat',
+    'Dog',
+    'Elephant'
+];
+
+// Functions
+
+function getRandomPhraseAsArray(phrases){
+
+
+};
+getRandomPhraseAsArray(phrases);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
