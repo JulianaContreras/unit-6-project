c5f1ae611e17c77faf5aa8d0f28d3a31fba527a2
Juliana Contreras <2022jcontreras@arisehighschool.org>
2021-06-29 13:30:26 -0700

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,33 +1,56 @@
 // variables 
-const qwerty= document.getElementById('qwerty');
-const phrase= document.getElementById('phrase');
-const startGameBtn= document.querySelector('.btn_reset');
-var missed= 0;
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startGameBtn= document.querySelector('.btn__reset');
+const overlay = document.querySelector('#overlay');
+var missed = 0;
 
-// Event Listeners
-
+// Start game event listener
 startGameBtn.addEventListener('click', ()=>{
-   
+   overlay.style.display = 'none';
 });
 
 // Arrays
 
-phrases= [
-    'Animal',
-    'Bunny',
-    'Cat',
-    'Dog',
-    'Elephant'
+phrases = [
+    "animals are very interesting creatures",
+    "bunnies can hop really fast",
+    "cats can meow and hiss",
+    "dogs can bark and play fetch",
+    "elephants are very cute and love to eat peanuts"
 ];
 
 // Functions
 
-function getRandomPhraseAsArray(phrases){
+function getRandomPhraseAsArray(array){
+    // get a random number to use as an index
+    const num = Math.floor(Math.random() * phrases.length);
+    // access the phrase at that index
+    const phraseString = array[num];
+    // split the phrase into a new array of characters and store it in characterArray
+    const characterArray = phraseString.split('');
+    // return characterArray
+
+
+    return characterArray;
+
+
+
+
+
 
 
 };
-getRandomPhraseAsArray(phrases);
+// getRandomPhraseAsArray(phrases);
 
+
+
+
+
+
+
+
+// Event Listeners
 
 
 
